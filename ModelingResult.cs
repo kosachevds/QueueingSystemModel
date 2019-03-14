45604be1a30dcee6ab4d7454db2bc6950dd575ab
Даниил Косачев <kdaniil@MCS>
2019-03-14 15:12:34 +0300

    Added MaxQueueSize

--- ModelingResult.cs
+++ ModelingResult.cs
@@ -5,5 +5,6 @@ namespace QueueingSystemModel
         public double AverageExistingTime { get; set; }
         public double AverageWaitingInQueue { get; set; }
         public double AverageDowntime { get; set; }
+        public double MaxQueueSize {get; set;}
     }
 }
