c8cfd6def363f0813c915b3ae5f900540d0c889a
Daniil Kosachev <kos-dan@mail.ru>
2019-03-17 12:38:57 +0300

    Added 'ToString' method

--- ModelingResult.cs
+++ ModelingResult.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace QueueingSystemModel
 {
     class ModelingResult
@@ -5,6 +7,14 @@ namespace QueueingSystemModel
         public double AverageSpentTime { get; set; }
         public double AverageWaitingInQueue { get; set; }
         public double AverageDowntime { get; set; }
-        public double MaxQueueSize {get; set;}
+        public double MaxQueueSize { get; set; }
+
+        public override string ToString()
+        {
+            return $@"Average Spent Time: {AverageSpentTime}, \
+            Average Waiting Time: {AverageWaitingInQueue}, \
+            Average Downtime: {AverageDowntime}, \
+            Max Queue Size: {MaxQueueSize}";
+        }
     }
 }
