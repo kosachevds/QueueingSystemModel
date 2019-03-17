79afd1f21abc9524a8ca180570fb907f35321caf
Daniil Kosachev <kos-dan@mail.ru>
2019-03-17 13:59:18 +0300

    Added methods 'ToSimpleString', 'ToStringWithFormat' Remade with mthod 'ToStringWithFormat'

--- ModelingResult.cs
+++ ModelingResult.cs
@@ -14,8 +14,19 @@ namespace QueueingSystemModel
         {
             var template = "Average Spent Time: {0}, Average Waiting Time: {1}, " +
                 "Average Downtime: {2}, Max Queue Size: {3}, Average queue size: {4}";
-            return String.Format(template,
-                AverageSpentTime, AverageWaitingInQueue, AverageDowntime, MaxQueueSize, AverageQueueSize);
+            return ToStringWithFormat(template);
+        }
+
+        public string ToSimpleString()
+        {
+            return ToStringWithFormat("{0} {1} {2} {3} {4}");
+        }
+
+        private string ToStringWithFormat(string formatString)
+        {
+            return String.Format(formatString,
+                AverageSpentTime, AverageWaitingInQueue, AverageDowntime,
+                MaxQueueSize, AverageQueueSize);
         }
     }
 }
